28aca9683c821ef247cb70b3de392d077097b90b
spitum <53660198+spitum@users.noreply.github.com>
2019-09-13 15:32:04 -0600

    Create how_many.py

---
+++ week 3/how_many.py
@@ -0,0 +1,10 @@
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    count = 0
+    for value in aDict.values():
+        count += len(value)
+    return count
